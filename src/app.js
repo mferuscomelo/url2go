a5f64f6702c0ff33a5737e4bf09005321e900ea1
Milan Ferus-Comelo <mferus-comelo@outlook.com>
2020-11-09 19:31:05 +0100

    Edited to work with Google Cloud

--- src/app.js
+++ src/app.js
@@ -1,29 +1,21 @@
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
-const dbURL = process.env.DATABASE;
-
-const doesUrlExist = (db, submittedKey) => db.collection('urls')
-  .findOne({ _id: submittedKey });
-
+const admin = require('firebase-admin');
 const express = require('express');
 var lessMiddleware = require('less-middleware');
 const path = require('path');
 const parse = require('body-parser');
 const urlModule = require('url');
-const { execSync } = require('child_process');
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://url2go-295111.firebaseio.com"
+});
+const db = admin.firestore();
 
 const app = express();
 
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(parse.json());
-
-MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(client => {
-    app.locals.db = client.db('url2go');
-    console.log('DB connection succesful at ' + dbURL);
-  })
-  .catch(() => console.error('DB connection failed :('));
 
 app.get('/', (req, res) => {
   const home = path.join(__dirname, 'public', 'index.html');
@@ -32,21 +24,18 @@ app.get('/', (req, res) => {
 
 app.get('/:key', (req, res) => {
   const key = req.params.key;
-  const { db } = req.app.locals;
-  doesUrlExist(db, key)
-    .then(doc => {
-      if (doc === null) return res.sendFile(path.join(__dirname, 'public', 'not-found.html'));
+
+  getUrl(key)
+    .then( (doc) => {
+      if (doc == null) 
+        return res.sendFile(path.join(__dirname, 'public', 'not-found.html'));
+
       res.redirect(doc.url)
     })
     .catch(console.error);
 });
 
 app.post('/create-url', async (req, res) => {
-  // for debugging
-  console.log('Url submitted: ', req.body.url);
-  console.log('Key submitted: ', req.body.key);
-  console.log('expireTime submitted: ', req.body.expireTime);
-
   // Catch passed information
   let expireTime = req.body.expireTime;
   let key = req.body.key;
@@ -58,25 +47,22 @@ app.post('/create-url', async (req, res) => {
       return res.status(400).send({error: 'invalid URL'});
   }
 
-  const { db } = req.app.locals;
-
-  await doesUrlExist(db, key)
-    .then( () => {
+  await getUrl(key)
+    .then( (doc) => {
       if(doc != null)
         return res.status(400).send({error: 'entry already exists'});
     })
-    .catch(console.error);
+    .catch( (error) => {
+      console.error(error);
+    });
 
-  const dbUrls = db.collection('urls');
   var currentDate = new Date();
 
-  dbUrls.insertOne(
-    {
-      _id: key,
-      url: url.href,
-      expireAt: new Date(currentDate.getTime() + expireTime*60000),
-    }
-  )
+  db.collection('urls').doc(key).set({
+    key: key,
+    url: url.href,
+    // expireAt: new admin.firestore.Timestamp.fromDate(new Date(Date.now() + ((expireTime) / (60 * 24) )))
+  })
   .then( () => {
     return res.status(200).send();
   })
@@ -85,7 +71,11 @@ app.post('/create-url', async (req, res) => {
   });
 });
 
-// app.set('port', process.env.PORT || 4100);
 const server = app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
   console.log(`Express running at PORT ${server.address().port}`);
 });
+
+async function getUrl(key) {
+  const doc = await db.collection('urls').doc(key).get();
+  return doc.exists ? doc.data() : null;
+}
