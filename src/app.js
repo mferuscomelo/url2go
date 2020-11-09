8ceff43e4442827c219dba472a467a68cfb0d3e3
Milan Ferus-Comelo <mferus-comelo@outlook.com>
2020-11-09 22:55:33 +0100

    Added comments

--- src/app.js
+++ src/app.js
@@ -1,55 +1,78 @@
+// Firebase for the database
 const admin = require('firebase-admin');
+
+// Express.js
 const express = require('express');
-var lessMiddleware = require('less-middleware');
+
+// Used to convert .less styles to .css (for convenience)
+const lessMiddleware = require('less-middleware');
+
 const path = require('path');
 const parse = require('body-parser');
 const urlModule = require('url');
 
+// Initialize firebase and connect to the correct database
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: "https://url2go-295111.firebaseio.com"
 });
 const db = admin.firestore();
 
+// Initialize the app
 const app = express();
 
+// Convert .less styles to .css
 app.use(lessMiddleware(path.join(__dirname, 'public')));
+
+// Make Express use the /public directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(parse.json());
 
+// Home Page
 app.get('/', (req, res) => {
   const home = path.join(__dirname, 'public', 'index.html');
   res.sendFile(home);
 });
 
+// Page with key
 app.get('/:key', (req, res) => {
+  // Get the key from the url
   const key = req.params.key;
 
+  // Check if the url2go exists in database
   getUrl(key)
     .then( (doc) => {
       if (doc == null) 
+        // Url2Go doesn't exist
         return res.sendFile(path.join(__dirname, 'public', 'not-found.html'));
 
+      // Route user to not-found.html page
       res.redirect(doc.url)
     })
-    .catch(console.error);
+    .catch( (error) => {
+      console.error(error);
+    });
 });
 
+// Create a new Url2Go
 app.post('/create-url', async (req, res) => {
   // Catch passed information
   let expireTime = req.body.expireTime;
   let key = req.body.key;
   let url;
 
+  // Check if the url is a proper url
   try {
       url = urlModule.parse(req.body.url);
   } catch (err) {
       return res.status(400).send({error: 'invalid URL'});
   }
 
+  // Check if the URL exists
   await getUrl(key)
     .then( (doc) => {
       if(doc != null)
+        // The Url2Go already exists
         return res.status(400).send({error: 'entry already exists'});
     })
     .catch( (error) => {
@@ -58,6 +81,7 @@ app.post('/create-url', async (req, res) => {
 
   var currentDate = new Date();
 
+  // Since the Url2Go doesn't exist, create a new one
   db.collection('urls').doc(key).set({
     key: key,
     url: url.href,
--- src/public/main.js
+++ src/public/main.js
@@ -1,13 +1,26 @@
+// Get the form component so that we can listen for the submit event
 const form = document.querySelector('form');
+
+// Get the input for the long URL so that we can query the value
 const urlInput = document.querySelector('#url-input');
+
+// Get the input for the key so that we can query the value
 const keyInput = document.querySelector('#key-input');
+
+// Get the button to display the short URL so that the user can copy it to clipboard
 const urlButton = document.querySelector('#short-url');
+
+// Get the success dialog element so that we can show it
 const successDialog = document.querySelector('.success-dialog');
+
+// Get the error dialog element so that we can show it
 const errorDialog = document.querySelector('.error-dialog');
 
+// Reset the input fields on page load
 urlInput.value = '';
 keyInput.value = '';
 
+// Listen for the input event on the urlInput element so that we can remove the protocol from the URL
 urlInput.addEventListener('input', (event) => {
     var protocol = event.target.value.match(/^https?:\/\//, '');
 
@@ -19,16 +32,17 @@ urlInput.addEventListener('input', (event) => {
     urlInput.value = event.target.value.replace(/^https?:\/\//, '');
 });
 
+// Listen for the submit event 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     if(urlInput.value == '' || urlInput.value == null) {
+        // Error, show error dialog
         error();
         return;
     }
 
-    // const expireSelected = document.querySelector('.expire');
-
+    // Cretae new Url2Go
     fetch('/create-url', {
         method: 'POST',
         headers: {
@@ -42,18 +56,13 @@ form.addEventListener('submit', (event) => {
         })
     })
         .then( (response) => {
-            // response.json();
-            // console.log(response);
-
-            // while (result.hasChildNodes()) {
-            //     result.removeChild(result.lastChild);
-            // }
-
             if(response.status == 200) {
+                // Success, show success dialog
                 success();
 
                 document.querySelector('#short-url').innerHTML = 'url2go.org/' + keyInput.value;
             } else if(response.status == 400) {
+                // Error, show error dialog
                 error();
 
                 document.querySelector('#error-message').innerHTML = 'Etwas ist schief gelaufen. Bitte versuche es erneut.'
